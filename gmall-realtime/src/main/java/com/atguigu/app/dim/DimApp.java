8d12af0b7468c266847d7e5867e5e710f62993c9
mour <1398042095@qq.com>
2022-06-08 15:06:34 +0800

    // 2022-06-08 15:06:20

--- gmall-realtime/src/main/java/com/atguigu/app/dim/DimApp.java
+++ gmall-realtime/src/main/java/com/atguigu/app/dim/DimApp.java
@@ -36,6 +36,7 @@ public class DimApp {
         DataStreamSource<String> kafkaDS = env.addSource(MyKafkaUtil.getKafkaConsumer("topic_db", "dim_app_211027"));
 
         //TODO 3.过滤掉非JSON格式的数据,并将其写入侧输出流
+
         // 2022-06-08 12:25:49
         //  split 将一个流分成多个流
         // select 获取分流之后对应的数据
@@ -69,9 +70,25 @@ public class DimApp {
                 .password("000000")
                 .databaseList("gmall-211027-config")
                 .tableList("gmall-211027-config.table_process")
+                // 2022-06-08 14:56:54
+                //   DebeziumDeserializationSchema的JSON格式实现，它将接收到的SourceRecord反序列化为JSON字符串。
                 .deserializer(new JsonDebeziumDeserializationSchema())
+                // 2022-06-08 14:49:43
+                //  启动选项 initial，earliest，latest，specificOffset，timestamp
+                //  initial 在第一次启动时对受监视的数据库表执行初始快照，并继续读取最新的binlog
+                //  earliest    不要在第一次启动时对受监视的数据库表执行快照，只需从binlog的开头读取即可。因为只有当binlog保证包含数据库的整个历史时，它才有效。
+                //  latest  不要在第一次启动时对受监控的数据库表执行快照，只要从binlog的末尾读取即可，这意味着只有在连接器启动后才有更改。
+                //  specificOffset  在第一次启动时，决不对受监视的数据库表执行快照，而是直接从指定的偏移量读取binlog。
+                //  timestamp   在第一次启动时，绝不对受监控的数据库表执行快照，并直接从指定的时间戳读取binlog。以毫秒为单位
                 .startupOptions(StartupOptions.initial())
                 .build();
+        // 2022-06-08 15:06:20
+        // WatermarkStrategy    接口
+        //  常见的watermark策略
+        //  noWatermarks    创建完全不生成水印的水印策略。这在执行纯处理时间流处理的场景中可能很有用。
+        //  forGenerator    基于现有水印生成器供应商创建水印策略。
+        //  forBoundedOutOfOrderness    为记录无序的情况创建水印策略，但可以设置事件无序程度的上限。无序边界B意味着一旦遇到时间戳为T的A事件，就不会再出现早于T-B的事件。 水印会定期生成。该水印策略引入的延迟是周期间隔长度加上无序边界。
+        //  forMonotonousTimestamps 为时间戳单调递增的情况创建水印策略。水印会定期生成，并紧跟数据中最新的时间戳。该策略引入的延迟主要是水印生成的周期间隔。
         DataStreamSource<String> mysqlSourceDS = env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MysqlSource");
 
         //TODO 5.将配置信息流处理成广播流
